17ad2b3a80730f5fa97355166f399ecc971a116e
Christian Klüh <60430668+chrisKlueh@users.noreply.github.com>
2021-07-08 15:03:12 +0200

    Add proper repositioning of non-player objects colliding with ForceObjectBarrier

--- Assets/ForceObjectBarrier.cs
+++ Assets/ForceObjectBarrier.cs
@@ -22,8 +22,34 @@ public class ForceObjectBarrier : MonoBehaviour
         if(other.gameObject.tag == "GrabbableObject")
         {
             //UnityEngine.Debug.Log("ForceObjectBarrier: Suitable object found");
-            ResetObjectPosition(other.gameObject.GetInstanceID());
-            
+            //ResetObjectPosition(other.gameObject.GetInstanceID());
+            Reposition(other.transform.position, other.gameObject);
+        }
+    }
+
+    void Reposition(Vector3 respectivePos, GameObject targetGameObject)
+    {
+        //UnityEngine.Debug.Log("ForceObjectBarrier: "+gameObject.transform.position.x+","+ gameObject.transform.position.y + ","+ gameObject.transform.position.z);
+        //targetGameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + 1);
+       // Vector3 relativePoint = gameObject.transform.InverseTransformPoint(0, 0, 0);
+
+
+
+        //float speed = 1.0f;
+
+
+        Vector3 targetDirectionLocal = gameObject.transform.InverseTransformPoint(targetGameObject.transform.position);
+
+        if (targetDirectionLocal.z < 0)
+        {
+            Debug.Log("ForceObjectBarrier: Target hit left side. Moving target further to the left.");
+            targetGameObject.transform.position = transform.TransformPoint(Vector3.back * 2);
+        }
+        else if (targetDirectionLocal.z > 0)
+        {
+            Debug.Log("ForceObjectBarrier: Target hit right side. Moving target further to the right.");
+            targetGameObject.transform.position = transform.TransformPoint(Vector3.forward * 2);
+
         }
     }
 
