77c690dcaa78d208882c24b6e57146cc18c1df48
Christian Klüh <60430668+chrisKlueh@users.noreply.github.com>
2021-07-21 01:46:25 +0200

    Tidy up DebugScene, Add a world border trigger to RespawnManager and modify its behavior

--- Assets/Scripts/RespawnManager.cs
+++ Assets/Scripts/RespawnManager.cs
@@ -5,14 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class RespawnManager : MonoBehaviour
 {
-    public List<GameObject> respawningObjectsList = new List<GameObject>();
+    public GameObject respawnablesParentObject;
+    public AudioClip sound_objectRespawned;
+    
+    private AudioSource source;
+    private List<GameObject> respawningObjectsList = new List<GameObject>();
+
     private Dictionary<int, GameObjectInit> initialPositions = new Dictionary<int, GameObjectInit>();
 
     // Start is called before the first frame update
     void Start()
     {
         EventsManager.instance.ResetObject += HandleResetObject;
+        this.source = gameObject.AddComponent<AudioSource>();
         //UnityEngine.Debug.Log("RespawnManager: start");
+        FillList();
         SaveInitialPositions();
     }
 
@@ -22,12 +29,22 @@ public class RespawnManager : MonoBehaviour
         
     }
 
+    void FillList()
+    {
+        Transform[] allChildren = respawnablesParentObject.GetComponentsInChildren<Transform>();
+        foreach (Transform child in allChildren)
+        {
+            this.respawningObjectsList.Add(child.gameObject);
+        }
+
+    }
+
     void SaveInitialPositions()
     {
         //UnityEngine.Debug.Log("RespawnManager: Saving initial positions");
         foreach (GameObject go in respawningObjectsList)
         {
-            UnityEngine.Debug.Log("RespawnManager: Added object id " + go.GetInstanceID() + " to dictionary");
+            //UnityEngine.Debug.Log("RespawnManager: Added object id " + go.GetInstanceID() + " to dictionary");
             this.initialPositions.Add(go.GetInstanceID(), new GameObjectInit(go));
         }
     }
@@ -36,7 +53,27 @@ public class RespawnManager : MonoBehaviour
     {
         this.initialPositions[instanceId].gameObject.transform.position = this.initialPositions[instanceId].initialPosition;
     }
-
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        int instanceId = other.gameObject.GetInstanceID();
+        //Debug.Log("RespawnManager: Object " + instanceId + " fell out of the world.");
+        
+        if (this.initialPositions.ContainsKey(instanceId))
+        {
+            //Debug.Log("RespawnManager: Respawning Object " + instanceId);
+            this.initialPositions[instanceId].gameObject.transform.position = this.initialPositions[instanceId].initialPosition;
+            this.source.PlayOneShot(sound_objectRespawned);
+        }
+        else if (other.gameObject.tag == "Player")
+        {
+            // reset scene
+            Debug.Log("RespawnManager: Player fell out of the world. Reloading Scene.");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            this.source.PlayOneShot(sound_objectRespawned);
+        }
+    }
 }
 
 public class GameObjectInit
