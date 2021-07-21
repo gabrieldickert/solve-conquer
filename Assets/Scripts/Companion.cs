e5e844b2cb9b8a9cfa940751d9fde8d790ce606b
Christian Klüh <60430668+chrisKlueh@users.noreply.github.com>
2021-07-21 15:34:35 +0200

    Prepare for target aiming

--- Assets/Scripts/Companion.cs
+++ Assets/Scripts/Companion.cs
@@ -8,16 +8,40 @@ public class Companion : MonoBehaviour
     //NavMesh Agent variable
     NavMeshAgent agent;
 
+    private Renderer companionRenderer;
+    private bool isFollowing = true;
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        companionRenderer = transform.GetChild(0).GetComponent<MeshRenderer>();
+        //Debug.Log(transform.GetChild(0).GetComponent<MeshRenderer>());
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Follow the player
-        agent.destination = transformToFollow.position;
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            this.isFollowing = !this.isFollowing;
+            companionRenderer.material.color = isFollowing ? Color.green : Color.red;
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.Two))
+        {
+            companionRenderer.material.color = Color.yellow;
+        }
+
+        if (this.isFollowing)
+        {
+            //Follow the player
+            agent.destination = transformToFollow.position;
+        } else
+        {
+            agent.destination = agent.transform.position;
+        }
+
+        
     }
 }
