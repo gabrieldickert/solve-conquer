6ea6ebf18f90cbd1280b8206f923cc8d105b6d83
gabrieldickert <59055689+gabrieldickert@users.noreply.github.com>
2021-07-30 20:08:36 +0200

    making spaceshilds opening at start of scene

---
+++ Assets/SpaceshipController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipController : MonoBehaviour
+{ 
+
+    private List<GameObject> ShieldList = new List<GameObject>();
+    private const string ShieldTag = "SpaceshipShield";
+    private const string Bridgename = "Bridge";
+    public const float SHIELD_Y_OFFSET = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Transform bridge = this.gameObject.transform.Find(Bridgename);
+        foreach (Transform child in bridge)
+        {
+            if (child.tag == ShieldTag)
+                child.gameObject.transform.position = new Vector3(child.gameObject.transform.position.x, child.gameObject.transform.position.y + SHIELD_Y_OFFSET, child.gameObject.transform.position.z);
+               ShieldList.Add(child.gameObject);
+        }
+
+
+
+
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
