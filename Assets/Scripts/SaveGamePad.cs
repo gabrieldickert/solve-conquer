edde019aeb44ea16ea3be247f0da0c54cba6f16c
Julian Schuster <julian.schuster@informatik.hs-fulda.de>
2021-08-15 03:14:56 +0200

    assigned save message to camera + fixes to save function

--- Assets/Scripts/SaveGamePad.cs
+++ Assets/Scripts/SaveGamePad.cs
@@ -7,17 +7,27 @@ public class SaveGamePad : MonoBehaviour
     public Player player;
     public Companion companion;
     public GameObject FloatingTextPrefab;
+    public Transform centerEyeAnchor;
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collider)
     {
-        SaveSystem.SaveGame(player, companion);
-
-        ShowFloatingText();
+        if(collider.gameObject.tag == "Player")
+        {
+            //Speichert erst wenn kein FloatingText(Clone) gefunden wird, d.h. alle 3 sec ist es möglich neu zu speichern (Zeit ist einstellbar im FloatingTexts script)
+            //Verhindert das spammen der Speichern Funktion und der zugehörigen Nachricht
+            if(centerEyeAnchor.Find("FloatingText(Clone)") == null)
+            {
+                SaveSystem.SaveGame(player, companion);
+                ShowFloatingText();
+            }
+         
+        }
     }
 
     public void ShowFloatingText()
     {
-        Instantiate(FloatingTextPrefab, new Vector3(companion.transform.position.x, companion.transform.position.y + 2.5f, companion.transform.position.z) , Quaternion.identity, companion.transform);
+        //Instantiate(FloatingTextPrefab, new Vector3(companion.transform.position.x, companion.transform.position.y + 2.5f, companion.transform.position.z) , Quaternion.identity, companion.transform);
+        Instantiate(FloatingTextPrefab, new Vector3(centerEyeAnchor.transform.position.x, centerEyeAnchor.transform.position.y, centerEyeAnchor.transform.position.z + 5), Quaternion.identity, centerEyeAnchor.transform);
     }
 
 }
