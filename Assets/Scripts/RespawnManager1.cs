ac126ed732a39fdf2e2ba4fddc7c0e16181845fe
gabrieldickert <59055689+gabrieldickert@users.noreply.github.com>
2021-08-16 15:10:12 +0200

    improved script

--- Assets/Scripts/RespawnManager1.cs
+++ Assets/Scripts/RespawnManager1.cs
@@ -16,45 +16,14 @@ public class RespawnManager1 : MonoBehaviour
         foreach (GameObject stageobj in GameObject.FindGameObjectsWithTag("Stage"))
         {
             List<GameObject> levelobjs = new List<GameObject>(GameObject.FindGameObjectsWithTag("Level")).FindAll(g => g.transform.IsChildOf(stageobj.transform));
-
             this.RespawnStageLevels.Add(stageobj.name, new List<LevelInstance>());
+
             for (int i = 0; i < levelobjs.Count; i++)
             {
                 this.RespawnStageLevels[stageobj.name].Add(new LevelInstance(stageobj.name, levelobjs[i]));
             }
-            // this.RespawnStageLevels.Add(stageobj.name, new List<GameObject>(GameObject.FindGameObjectsWithTag("Level")).FindAll(g => g.transform.IsChildOf(stageobj.transform)));
-            //.Where(w=> w.transform.Find("Geometry")).ToList()
         }
 
-        foreach (string key in this.RespawnStageLevels.Keys)
-        {
-            foreach (LevelInstance o in this.RespawnStageLevels[key])
-            {
-                for (int i = 0; i < o.levelobj.transform.Find("Geometry").childCount; i++)
-                {
-                    //   if(o.levelobj.name.Equals("Level2"))
-                    //{
-                    // Debug.Log(o.levelobj.transform.Find("Geometry").transform.GetChild(i).gameObject.name);
-                    //Checking on Floor or not
-                    o.geometryObjList.Add(o.levelobj.transform.Find("Geometry").transform.GetChild(i).gameObject);
-                    // groupVec += o.levelobj.transform.Find("Geometry").transform.GetChild(i).gameObject.transform.position;
-                    //  }
-                }
-
-                /*
-                // Debug.Log(o.levelobj.name);
-                Bounds b = CreateBound(o.geometryObjList.ToArray());
-                GameObject go = new GameObject();
-                go.transform.position = b.center;
-                Vector3 respawncollidersize = new Vector3(b.size.x + 2 * b.extents.x, b.size.y + 2 * b.extents.y, b.size.z + 2 * b.extents.x);
-                go.AddComponent<BoxCollider>().size = respawncollidersize;
-                //Includes Y-Offset
-                go.GetComponent<BoxCollider>().center = new Vector3(0, (b.max.y - b.min.y) * -1 - 5, 0);
-                go.GetComponent<BoxCollider>().isTrigger = true;
-                //Adding Respawnlistener to listen on Collision
-                go.AddComponent<RespawnListener>();*/
-            }
-        }
     }
 
     private void Instance_ResetPlayer()
@@ -74,17 +43,6 @@ public class RespawnManager1 : MonoBehaviour
     {
     }
 
-    private Bounds CreateBound(GameObject[] gos)
-    {
-        if (gos.Length == 0)
-            return new Bounds();
-        if (gos.Length == 1)
-            return new Bounds();
-        Bounds bounds = new Bounds(gos[0].transform.position, Vector3.zero);
-        for (var i = 1; i < gos.Length; i++)
-            bounds.Encapsulate(gos[i].transform.position);
-        return bounds;
-    }
 }
 
 public class LevelInstance
@@ -92,25 +50,22 @@ public class LevelInstance
     public string stage { get; set; }
     public GameObject levelobj { get; set; }
     public BoxCollider RespawnTrigger { get; set; }
-    public List<GameObject> geometryObjList { get; set; }
     public List<RespawnObject> respawnObjList { get; set; }
-
     public LevelInstance(string stage, GameObject lvl)
     {
         this.stage = stage;
         this.levelobj = lvl;
-        this.geometryObjList = new List<GameObject>();
         this.respawnObjList = new List<RespawnObject>();
         this.FindRespawnObjectsInLevel();
     }
 
     private void FindRespawnObjectsInLevel()
     {
-        if (this.levelobj.transform.Find("Respawnables") != null)
+        if (this.levelobj.transform.Find("Respawn") != null)
         {
-            for (int i = 0; i < this.levelobj.transform.Find("Respawnables").childCount; i++)
+            for (int i = 0; i < this.levelobj.transform.Find("Respawn").transform.Find("Respawnables").childCount; i++)
             {
-                GameObject o = this.levelobj.transform.Find("Respawnables").GetChild(i).gameObject;
+                GameObject o = this.levelobj.transform.Find("Respawn").transform.Find("Respawnables").GetChild(i).gameObject;
 
                 respawnObjList.Add(new RespawnObject(o));
             }
