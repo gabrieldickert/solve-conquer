76f32f54a2f106bde1e5ffb0a114f5360b6532c0
Christian Klüh <60430668+chrisKlueh@users.noreply.github.com>
2021-08-23 13:44:39 +0200

    Remove an unused variable from AmbientsoundManager and AmbientEntityScript scripts, give hackable Barriers a different color, connect acitvators in Stage2-Level4

--- Assets/Scripts/Barrier.cs
+++ Assets/Scripts/Barrier.cs
@@ -33,7 +33,14 @@ public class Barrier : MonoBehaviour
         // Get material's starting color value.
         m_Color = m_Material.color;
         
-
+        if(gameObject.tag == "HackableObject")
+        {
+            Debug.Log(m_Color.r);
+            Debug.Log(m_Color.g);
+            Debug.Log(m_Color.b);
+            Debug.Log(m_Color.a);
+            m_Color = new Color(m_Color.r, 1f, 1f, m_Color.a);
+        }
 
         //Initialize Barrier (isBridge ? disable collision and fade color out : enable collision and fade color in)
         if (!isActiveOnStart)
