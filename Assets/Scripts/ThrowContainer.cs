64e66924f00a316b7775aab646a2ad9cdbccf522
gabrieldickert <59055689+gabrieldickert@users.noreply.github.com>
2021-08-25 19:14:16 +0200

    added throwable object

--- Assets/Scripts/ThrowContainer.cs
+++ Assets/Scripts/ThrowContainer.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class ThrowContainer : MonoBehaviour
 {
+
+    private Dictionary<GameObject,Vector3> ThrowablesDic;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Check for all Throwables and get initpos
+        this.ThrowablesDic = new Dictionary<GameObject, Vector3>();
+
+        Transform t = this.gameObject.transform.Find("Throwables");
+     
+        for(int i = 0; i < t.childCount;i++)
+        {
+
+            ThrowablesDic.Add(t.GetChild(i).gameObject, t.GetChild(i).position);
+
+         
+        }
+
+
     }
 
     // Update is called once per frame
