b038f052a146a6fb04fccf2075fa06058fe43395
Christian Klüh <60430668+chrisKlueh@users.noreply.github.com>
2021-08-27 17:11:33 +0200

    Add hit indicators to StandingTarget/ ThrowTargetListener

--- Assets/Scripts/ThrowTargetListener.cs
+++ Assets/Scripts/ThrowTargetListener.cs
@@ -8,11 +8,15 @@ public class ThrowTargetListener : MonoBehaviour
     public bool isActive = false;
     public int trigger1 = 0;
     public int trigger2 = 0;
-   
+
+    public MeshRenderer lightRenderer1 = null;
+    public MeshRenderer lightRenderer2 = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        //gameObject.GetComponent<Renderer>().material.color = new Color(255, 0, 0, 1f);
+        lightRenderer1.material.color = Color.red;
+        lightRenderer2.material.color = Color.red;
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -21,13 +25,15 @@ public class ThrowTargetListener : MonoBehaviour
         {
             if (!isActive)
             {
-                //gameObject.GetComponent<Renderer>().material.color = new Color(0, 255, 0, 1f);
+                lightRenderer1.material.color = Color.green;
+                lightRenderer2.material.color = Color.green;
                 EventsManager.instance.OnThrowableTargetEnable(trigger1);
                 EventsManager.instance.OnThrowableTargetEnable(trigger2);
                 isActive = true;}
             else if (isActive)
             {
-                //gameObject.GetComponent<Renderer>().material.color = new Color(255, 0, 0, 1f);
+                lightRenderer1.material.color = Color.red;
+                lightRenderer2.material.color = Color.red;
                 EventsManager.instance.OnThrowableTargetDisable(trigger1);
                 EventsManager.instance.OnThrowableTargetDisable(trigger2);
                 isActive = false;
