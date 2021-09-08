0effb9fcf643948e9d04d19cd218c58982e515a7
gabrieldickert <59055689+gabrieldickert@users.noreply.github.com>
2021-09-08 19:21:43 +0200

    added door opening logic at landing

---
+++ Assets/SpaceShipLandingSceneScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class SpaceShipLandingSceneScript : MonoBehaviour
+{
+
+    private PlayableDirector pd;
+    private GameObject Player;
+    public GameObject door;
+    private Animator doorAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pd = this.GetComponent<PlayableDirector>();
+        pd.stopped += OnPlayableDirectorStopped;
+        Player = GameObject.FindWithTag("Player");
+        Player.GetComponent<Rigidbody>().isKinematic = true;
+        this.doorAnimator = this.door.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    void OnPlayableDirectorStopped(PlayableDirector aDirector)
+    {
+
+        Player.GetComponent<Rigidbody>().isKinematic = false;
+        Player.transform.parent = null;
+        this.doorAnimator.SetBool("character_nearby", true);
+    }
+
+}
