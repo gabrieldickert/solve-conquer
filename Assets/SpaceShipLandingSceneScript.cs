c4c9776db0ab554ab70cb8b41090c3e2532f0f7e
gabrieldickert <59055689+gabrieldickert@users.noreply.github.com>
2021-09-08 20:10:27 +0200

    fixed disabling scripts which occured renderbugs

--- Assets/SpaceShipLandingSceneScript.cs
+++ Assets/SpaceShipLandingSceneScript.cs
@@ -17,7 +17,10 @@ public class SpaceShipLandingSceneScript : MonoBehaviour
         pd.stopped += OnPlayableDirectorStopped;
         Player = GameObject.FindWithTag("Player");
         Player.GetComponent<Rigidbody>().isKinematic = true;
+        Player.GetComponent<Rigidbody>().interpolation = RigidbodyInterpolation.Extrapolate;
         this.doorAnimator = this.door.GetComponent<Animator>();
+        
+        
     }
 
     // Update is called once per frame
@@ -32,6 +35,10 @@ public class SpaceShipLandingSceneScript : MonoBehaviour
         Player.GetComponent<Rigidbody>().isKinematic = false;
         Player.transform.parent = null;
         this.doorAnimator.SetBool("character_nearby", true);
+        Player.transform.Find("LocomotionController").GetComponent<LocomotionController>().enabled = true;
+        transform.Find("OVRCameraRig/TrackingSpace/LeftHandAnchor").gameObject.GetComponent<LineRenderer>().enabled = true;
+        transform.Find("OVRCameraRig/TrackingSpace/LeftHandAnchor").gameObject.GetComponent<CompanionAimHandler>().enabled = true;
+        Player.GetComponent<Rigidbody>().interpolation = RigidbodyInterpolation.None;
     }
 
 }
