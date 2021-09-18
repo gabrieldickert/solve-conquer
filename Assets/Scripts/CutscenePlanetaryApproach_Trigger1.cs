5e425eb21fdaabbefee46e6656866eca1b154de2
Christian Klüh <60430668+chrisKlueh@users.noreply.github.com>
2021-09-18 15:28:57 +0200

    Load PlanetScene after PlanetaryApproach

--- Assets/Scripts/CutscenePlanetaryApproach_Trigger1.cs
+++ Assets/Scripts/CutscenePlanetaryApproach_Trigger1.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 
 public class CutscenePlanetaryApproach_Trigger1 : MonoBehaviour
 {
@@ -21,6 +23,7 @@ public class CutscenePlanetaryApproach_Trigger1 : MonoBehaviour
         gameObject.GetComponent<CapsuleCollider>().enabled = false;
         gameObject.GetComponent<MeshRenderer>().enabled = false;
         previousTimeLine.GetComponent<PlayableDirector>().stopped += OnPlayableDirectorStopped;
+        timeLine.GetComponent<PlayableDirector>().stopped += OnLoadPlanetScene;
     }
 
     private void Update()
@@ -45,6 +48,28 @@ public class CutscenePlanetaryApproach_Trigger1 : MonoBehaviour
             gameObject.GetComponent<MeshRenderer>().enabled = true;
         }
             
+    }
+
+    void OnLoadPlanetScene(PlayableDirector aDirector)
+    {
+        Debug.Log("ALERT: TIMELINE STOPPED: LOAD PLANETSCENE");
+        StartCoroutine(LoadYourAsyncScene());
+    }
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("PlanetScene");
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
     }
 
     /*void OnDisable()
