ce737510d7dc740b639917527f2f527b55b1cd75
Christian Klüh <60430668+chrisKlueh@users.noreply.github.com>
2021-09-19 16:43:24 +0200

    fix another package related bug

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
-using System.Numerics;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Playables;
