a0cceeb6013a7ff0bd4ba4f295af17de2a02a249
julian-schuster <Schuster.Julian@web.de>
2021-09-20 03:19:10 +0200

    code cleanup + check if a save exists on Start() added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,39 +18,20 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
-        this.LoadGame();
+        GameData gd = SaveSystem.LoadGame();
+
+        if( gd != null)
+        {
+            this.LoadGame();
+        } else
+        {
+            landingCutscene.Play();
+        }
+        
     }
 
     public void LoadGame()
     {
-        //  GameData data = SaveSystem.LoadGame();
-
-        //Vector3 positionPlayer;
-        //positionPlayer.x = data.positionPlayer[0];
-        //positionPlayer.y = data.positionPlayer[1];
-        //positionPlayer.z = data.positionPlayer[2];
-        //this.transform.position = positionPlayer;
-
-        //Vector3 positionCompanion;
-        //positionCompanion.x = data.positionCompanion[0];
-        //positionCompanion.y = data.positionCompanion[1];
-        //positionCompanion.z = data.positionCompanion[2];
-        //companion.transform.position = positionCompanion;
-
-        /*  Vector3 positionPad;
-          positionPad.x = data.positionPad[0];
-          positionPad.y = data.positionPad[1];
-          positionPad.z = data.positionPad[2];
-
-          this.transform.position = positionPad;
-          Debug.Log(data.saveCompanionPosition);
-          if(data.saveCompanionPosition)
-          {
-              companion.GetComponent<NavMeshAgent>().enabled = false;
-              companion.transform.position = positionPad + new Vector3(1.5f, 0, 0);
-              companion.GetComponent<NavMeshAgent>().enabled = true;
-          }*/
-
 
         GameData gd = SaveSystem.LoadGame();
 
@@ -90,12 +71,7 @@ public class Player : MonoBehaviour
             player.transform.parent = plat.VisualTrigger1.transform;
             player.transform.position = plat.VisualTrigger1.GetComponent<MeshRenderer>().bounds.center;
 
-        } else
-        {
-            //play landing cutscene, if no savegame exists (= beginning of game)
-            landingCutscene.Play();
-        }
-
+        } 
 
         menu.SetActive(false);
         IngameMenu.showMenu = false;
