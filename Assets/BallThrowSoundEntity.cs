6b397dbabfbeafbd2a3d66c2a8c07ebfe7748935
gabrieldickert <59055689+gabrieldickert@users.noreply.github.com>
2021-09-21 19:49:52 +0200

    added dong sound for ball

--- Assets/BallThrowSoundEntity.cs
+++ Assets/BallThrowSoundEntity.cs
@@ -7,6 +7,8 @@ public class BallThrowSoundEntity : MonoBehaviour
 {
     public Sound SoundSrc;
     private bool isThrowing = false;
+    private bool HasFirstCol = false;
+    private float refSpeed = 0;
     private AudioSource AudioSrc;
     private Rigidbody Physics;
     private DistanceGrabbable DistanceGrab;
@@ -15,28 +17,42 @@ public class BallThrowSoundEntity : MonoBehaviour
     {
         this.AudioSrc = this.gameObject.GetComponent<AudioSource>();
         this.Physics = this.gameObject.GetComponent<Rigidbody>();
-        
+       
        // this.DistanceGrab = this.gameObject.GetComponent<DistanceGrabbable>();
-        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        Debug.Log("X:"+this.gameObject.GetComponent<Rigidbody>().velocity.x);
-        Debug.Log("Y:" + this.gameObject.GetComponent<Rigidbody>().velocity.y);
-        Debug.Log("Z:" + this.gameObject.GetComponent<Rigidbody>().velocity.z);*/
-
-        //Debug.Log("Speed" + this.gameObject.GetComponent<Rigidbody>().velocity.magnitude);
-
-        if(this.Physics.velocity.magnitude > 0) {
-
-    
-            //Ball has Speed
-            isThrowing = true;
-
+        if(this.Physics.velocity.magnitude > 1) {
 
         }
+        else
+        {
+
+            HasFirstCol = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        this.AudioSrc.Stop();
+        /*
+        if(!HasFirstCol) {
+
+            refSpeed = this.Physics.velocity.magnitude;
+
+            HasFirstCol = true;
+        }
+        */
+        this.AudioSrc.volume = 1;
+        this.AudioSrc.clip = this.SoundSrc.clipList[1];
+        this.AudioSrc.Play();
+
+
+      
     }
 }
