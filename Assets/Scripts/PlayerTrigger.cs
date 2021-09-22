1747fd50c0798c53d61d0b19d3bcad42d4f5451a
Christian Klüh <60430668+chrisKlueh@users.noreply.github.com>
2021-09-22 19:36:24 +0200

    connect timelines, introduce new layer

--- Assets/Scripts/PlayerTrigger.cs
+++ Assets/Scripts/PlayerTrigger.cs
@@ -4,13 +4,21 @@ public class PlayerTrigger : MonoBehaviour
 {
     public int triggerId = 0;
     public bool triggerOnlyOnce = true;
+    public bool sendSwitchEnable = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
             //Debug.Log("Trigger hit");
-            EventsManager.instance.OnPlayerEnteredTrigger(this.triggerId);
+            if(sendSwitchEnable)
+            {
+                EventsManager.instance.OnSwitchEnable(this.triggerId);
+            } else
+            {
+                EventsManager.instance.OnPlayerEnteredTrigger(this.triggerId);
+            }
+
             if(triggerOnlyOnce)
             {
                 gameObject.GetComponent<BoxCollider>().enabled = false;
