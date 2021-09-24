cc65313d956056766eb26e052d3f6e41fdd5f79f
julian-schuster <Schuster.Julian@web.de>
2021-09-24 19:12:18 +0200

    Update AudioManager.cs

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -37,7 +37,7 @@ public class AudioManager : MonoBehaviour
 
         currentTheme = UnityEngine.Random.Range(0, sounds[0].clipList.Length);
 
-        //StartCoroutine(FadeIn("Themes", currentTheme, 3));
+        StartCoroutine(FadeIn("Themes", currentTheme, 3));
 
     }
 
